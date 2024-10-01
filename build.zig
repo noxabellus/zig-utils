113f329eac8152f75aed5eafe73fb6294f21c03a
noxabellus <noxabellus@gmail.com>
2024-10-01 16:42:55 -0500

    fixes

--- build.zig
+++ build.zig
@@ -7,9 +7,26 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
 
     const mod = b.addModule("ZigUtils", .{
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
+
+    const build_utils = b.dependency("ZigBuildUtils", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const SnapshotWriter = build_utils.artifact("snapshot-writer");
+    const Templater = build_utils.artifact("templater");
+    const LibJoiner = build_utils.artifact("libjoiner");
+    const HeaderGen = build_utils.namedLazyPath("HeaderGen.zig");
+
+    b.default_step.dependOn(&b.addInstallArtifact(SnapshotWriter, .{}).step);
+    b.default_step.dependOn(&b.addInstallArtifact(Templater, .{}).step);
+    b.default_step.dependOn(&b.addInstallArtifact(LibJoiner, .{}).step);
+    b.default_step.dependOn(&b.addInstallFile(HeaderGen, "HeaderGen.zig").step);
+
 
     const extern_utils = b.dependency("ZigExternUtils", .{
         .target = target,
@@ -31,6 +48,7 @@ pub fn build(b: *std.Build) !void {
         .optimize = optimize,
     });
 
+    mod.addImport("ZigBuildUtils", build_utils.module("ZigBuildUtils"));
     mod.addImport("ZigExternUtils", extern_utils.module("ZigExternUtils"));
     mod.addImport("ZigMiscUtils", misc_utils.module("ZigMiscUtils"));
     mod.addImport("ZigTextUtils", text_utils.module("ZigTextUtils"));
